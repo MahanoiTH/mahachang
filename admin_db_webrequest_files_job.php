dd2880bd011fbe5fcdb73b524993442754d6c66f
MahanoiTH <127701246+MahanoiTH@users.noreply.github.com>
2024-01-30 22:55:54 +0700

    update dropzone edit img

---
+++ admin_db_webrequest_files_job.php
@@ -0,0 +1,38 @@
+<?php
+include('server.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $job_id = $_POST['job_id'];
+
+    $sql = "SELECT *
+            FROM job_attachment
+            WHERE job_attachment.active = 1 AND job_attachment.job_id = $job_id";
+
+    $result = $conn->query($sql);
+
+    // Check for errors in query execution
+    if ($result === false) {
+        echo "Error: " . $conn->error;
+    } else {
+        $data = array();
+
+        // Fetch data from the result set
+        while ($row = $result->fetch_assoc()) {
+            // Convert the comma-separated file URLs to an array
+            $formattedRow = array(
+                'id' => $row['id'],
+                'job_id' => $row['job_id'],
+                'file_name' => $row['file_url'],
+                // Add more columns as needed
+            );
+            $data[] = $formattedRow;
+        }
+
+        // Close the database connection
+        $conn->close();
+
+        // Convert the data array to JSON and echo it
+        echo json_encode($data);
+    }
+}
+?>
